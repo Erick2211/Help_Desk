c2ae1aa0db0a57fbfb85c022246196f2b091eb0c
Principal <erickrodrigez2007@gmail.com>
2024-12-08 20:37:59 -0600

    app.js

--- js/app.js
+++ js/app.js
@@ -51,23 +51,24 @@ formulario.addEventListener('submit', async(e) => {
 
 
 // Actualizar Filas
-	try {
-		const respuesta = await fetch('https://sheet.best/api/sheets/e1bf0443-8bca-42a4-afc2-4f36b4e8fbd5/3', {
-			method: 'PATCH',
-			mode: 'cors',
-			headers: {
-				'Content-Type': 'application/json'
-			},
-			body: JSON.stringify({
-				"Telefono": "000 000 000"
-			})
-		});
-
-		const contenido = await respuesta.json();
-		console.log(contenido);
-	} catch(error){
-		console.log(error);
-	}
+	
+// try {
+//		const respuesta = await fetch('https://sheet.best/api/sheets/e1bf0443-8bca-42a4-afc2-4f36b4e8fbd5/3', {
+//			method: 'PATCH',
+//			mode: 'cors',
+//			headers: {
+//				'Content-Type': 'application/json'
+//			},
+//			body: JSON.stringify({
+//				"Telefono": "000 000 000"
+//			})
+//		});
+//
+//		const contenido = await respuesta.json();
+//		console.log(contenido);
+//	} catch(error){
+//		console.log(error);
+//	}
 
 
 	// registro.classList.remove('activo');
